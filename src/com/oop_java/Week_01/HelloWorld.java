41c2a1cff5db0f6407d74da30b16fab22441eb27
Muhammad Shahid Bhatti <mshahidbhatti@gmail.com>
2022-09-13 16:24:37 +0500

    primitive types print functions

---
+++ src/com/oop_java/Week_01/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.oop_java.Week_01;
+
+// jdk
+// ide
+// source code
+
+public class HelloWorld {
+
+    public static void main(String args[]){
+
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/com/oop_java/Week_01/Lab01.java
@@ -0,0 +1,39 @@
+package com.oop_java.Week_01;
+
+/*Data types are divided into two groups:
+Primitive data types
+- includes byte , short , int , long , float , double , boolean and char.
+Non-primitive data types - such as String , Arrays and Classes*/
+public class Lab01 {
+
+    public static void main(String args[]){
+
+        byte b=10;
+        short s=20;
+        int i=100;
+        long l=1000;
+        float f=10.0f;
+        double d=100.0;
+        boolean b1=true;
+        char c='c';
+
+
+
+        System.out.println("Java is interesting");
+
+        System.out.printf("%n %-30s","WE are using jdk 15.0");
+
+        System.out.printf("%nIntelliJ is recomended editor%n");
+        System.out.printf("The value in %03d %n",b);
+        System.out.printf("The value in %d %n",s);
+        System.out.printf("The value in %d %n",i);
+        System.out.printf("The value in %d %n",l);
+        System.out.printf("The value in %.2f %n",f);
+        System.out.printf("The value in %.2f %n",d);
+        System.out.printf("The value in %b %n",b1);
+        System.out.printf("The value in %c %n",c);
+
+
+
+    }
+}
