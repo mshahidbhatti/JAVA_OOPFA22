e743dd60cf8dbfb8ca4bb97b5de36b5a77072d5b
Muhammad Shahid Bhatti <mshahidbhatti@gmail.com>
2022-09-14 11:22:39 +0500

    Section C contents

---
+++ src/com/oop_java/Week_01/BCS_C.java
@@ -0,0 +1,32 @@
+package com.oop_java.Week_01;
+
+public class BCS_C {
+    public static void main(String args[]){
+        byte b=10;
+        short s=100;
+        int i=100;
+        long l=1000;
+
+        float f=10.0f;
+        double d=100.0;
+
+        char c='c';
+        boolean b1= true; //false
+
+        System.out.println("Welcome to Java programing");
+        System.out.print("Java is interesting "+" java");
+        System.out.println();
+        System.out.printf("Number with integer %03d",1);
+        System.out.printf("Number with flaots %.3d",1);
+
+        System.out.printf("Number with string chara %-20s","1");
+
+
+
+
+
+
+
+        System.out.println("Hello World");
+    }
+}
